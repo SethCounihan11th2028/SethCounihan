8139f16ab3c1e98daa45cc359bf8211ce84ed0ee
SethCounihan11th2028 <837505@mystma.org>
2026-09-25 13:03:48 -0500

    Mad Lib

---
+++ Unit 2/SimpleMadlib.py
@@ -0,0 +1,16 @@
+# [Name] went to the [place] with [Person/animal].
+# First, they [verb] in their [Thing],
+# Then, they saw [noun] and [verb].
+# [Adverb], they built a [noun] out of [noun].
+
+name = input("Give me a name \n")
+place = input("Give me a place \n")
+person_or_animal = input("Give me a person or animal \n")
+past_tense_verb = input("Give me a past tense verb \n ")
+thing = input("Give me a thing \n ")
+noun = input("Give me a noun \n ")
+adverb = input("Give me an adverb \n ")
+noun_two = input("Give me another noun \n ")
+noun_three = input("Give me one more noun \n ")
+
+print(name + " went to " + place + " with " + person_or_animal + ". First, they " + verb + " in their " + thing + ". Then, they saw " + noun_two + ". " + adverb + ", they built a " + noun_two + " out of " + noun_three + "." )
